b59797a85b17e9697a0a593049cb33e7a84bfd75
Smriti-Prajapati <smritiprajapati15@gmail.com>
2026-08-24 00:14:21 +0530

    backend setup

---
+++ backend/src/main/java/com/parcelgo/ParcelGoApplication.java
@@ -0,0 +1,11 @@
+package com.parcelgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelGoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ParcelGoApplication.class, args);
+    }
+}
